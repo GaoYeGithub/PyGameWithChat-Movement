e9bd7e2661385f40fe16e06449c0ec1670c502f7
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-06-28 20:18:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import pygame, sys
+from pygame.locals import QUIT
+from Cameragroup import *
+
+display_text = ''
+
+class Bartender(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.transform.scale(pygame.image.load('graphics/bartender.png').convert_alpha(), (100, 100))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.touched = False
+        self.display_text = 'Talk to the bartender!'
+
+    def update(self):
+        global interacting_with_bartender, display_text
+        if self.rect.colliderect(player.rect):
+            if not self.touched:
+                self.touched = True
+                interacting_with_bartender = True
+                display_text = 'hello there'
+        else:
+            if self.touched:
+                self.touched = False
+                interacting_with_bartender = False
+                display_text = ''
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.transform.scale(pygame.image.load('graphics/player.png').convert_alpha(), (50, 50))
+        self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        self.rect.center += self.direction * self.speed
+
+pygame.init()
+WIDTH, HEIGHT = 800, 500
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 176, 240)
+screen = pygame.display.set_mode((800, 500))
+clock = pygame.time.Clock()
+pygame.event.set_grab(True)
+
+pygame.font.init()
+pygame.display.set_caption('Hello World!')
+pygame.font.init()
+font = pygame.freetype.SysFont(None, 36)
+camera_group = CameraGroup()
+player = Player((640, 360), camera_group)
+
+output_box = pygame.Rect(50, 50, WIDTH - 100, 100)
+output_color = BLUE
+output_text = display_text
+
+bartender = Bartender((450, 100), camera_group)
+
+input_box = pygame.Rect(50, HEIGHT - 100, WIDTH - 100, 100)
+input_color_inactive = BLUE
+input_color_active = WHITE
+input_color = input_color_inactive
+
+active = False
+text = ''
+interacting_with_bartender = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+        if event.type == pygame.MOUSEWHEEL:
+            camera_group.zoom_scale += event.y * 0.03
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if interacting_with_bartender and input_box.collidepoint(event.pos):
+                active = not active
+                if active:
+                    text = ''
+            else:
+                active = False
+            input_color = input_color_active if active else input_color_inactive
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    output_text = 'hello there'
+                    text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+
+    screen.fill('#71ddee')
+    camera_group.update()
+    camera_group.custom_draw(player)
+    pygame.draw.rect(screen, output_color, output_box, 0, border_radius=20)
+    font.render_to(screen, (output_box.x + 10, output_box.y + 10), output_text, BLACK)
+    if interacting_with_bartender:
+        pygame.draw.rect(screen, input_color, input_box, 0, border_radius=20)
+        font.render_to(screen, (input_box.x + 10, input_box.y + 10), text, BLACK)
+
+    pygame.display.update()
+    clock.tick(60)
